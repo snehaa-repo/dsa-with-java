dc87531e2cf802ccdbafbf1729a2057a0b123255
snehaa <dev.snehaanakil@gmail.com>
2025-12-24 15:46:16 +0000

    first hello world program-println & print

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+//java hello worl printing :
 public class Main {
     public static void main(String[] args) {
-        System.out.println("DSA practice with Java");
+        System.out.println("Hello World java");
+        System.out.print("Hello World java");
+        System.out.println("Hello World java");
     }
 }
